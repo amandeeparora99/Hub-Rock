5a3e2fd9c5b8bf4c74d4fda7664b9c85fd99f1e2
my friend <melhajjami8@boscdelacoma.cat>
2020-07-27 23:58:32 +0200

    Carpeta i module de reusables creat i funcionant

--- HubnRock/src/app/authentication/authentication.module.ts
+++ HubnRock/src/app/authentication/authentication.module.ts
@@ -1,21 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ReusableModule } from '../reusable/reusable.module';
+
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';  
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
     RegisterComponent,
-    LoginComponent,
-    FooterComponent,
-    HeaderComponent
+    LoginComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReusableModule
   ]
 })
 export class AuthenticationModule { }
