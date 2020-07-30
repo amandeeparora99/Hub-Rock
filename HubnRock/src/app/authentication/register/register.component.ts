d6bb8ef8b9a66f1b06cfdcf06fb8e89e8074e543
Aman <aaman.arora99@gmail.com>
2020-07-30 16:57:42 +0200

    Reactive forms aplicats al register - progres

--- HubnRock/src/app/authentication/register/register.component.ts
+++ HubnRock/src/app/authentication/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -12,17 +12,80 @@ export class RegisterComponent implements OnInit {
   accountType: number;
   register: number = 0;
 
-  constructor() { }
+  validationMessages = {
+    'nomEmpresa': {
+      'required': 'Introdueix un nom d\'empresa',
+      'minlength': 'Massa curt',
+      'maxlength': 'Massa llarg'
+    },
+    'nomResponsable': {
+      'required': 'Introdueix un nom del responsable'
+    },
+    'nomCorreu': {
+      'required': 'Introdueix un correu'
+    },
+    'nomContrasenya': {
+      'required': 'Introdueix una contrasenya'
+    },
+    'nomRepeteixContrasenya': {
+      'required': 'Introdueix una contrasenya'
+    },
+    'nomNifEmpresa': {
+      'required': 'Introdueix un NIF empresa'
+    }
+  }
+
+  formErrors = {
+    'nomEmpresa': '',
+    'nomResponsable': '',
+    'nomCorreu': '',
+    'nomContrasenya': '',
+    'nomRepeteixContrasenya': '',
+    'nomNifEmpresa': ''
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.registerForm = new FormGroup({
-      nomEmpresa: new FormControl(),
-      nomResponsable: new FormControl(),
-      nomCorreu: new FormControl(),
-      nomContrasenya: new FormControl(),
-      nomRepeteixContrasenya: new FormControl(),
-      nomNifEmpresa: new FormControl()
+    this.registerForm = this.fb.group({
+      nomEmpresa: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(10)]],
+      nomResponsable: ['', Validators.required],
+      nomCorreu: ['', Validators.required],
+      nomContrasenya: ['', Validators.required],
+      nomRepeteixContrasenya: ['', Validators.required],
+      nomNifEmpresa: ['', Validators.required]
+    })
+
+    this.registerForm.valueChanges.subscribe((data) => {
+      this.logValidationErrors(this.registerForm)
     });
+  }
+
+  logValidationErrors(group: FormGroup = this.registerForm): void {
+    Object.keys(group.controls).forEach((key: string) => {
+      const abstractControl = group.get(key);
+      if (abstractControl instanceof FormGroup) {
+        this.logValidationErrors(abstractControl);
+      }
+      else {
+        this.formErrors[key] = '';
+        if (abstractControl && !abstractControl.valid && 
+          (abstractControl.touched || abstractControl.dirty)) {
+          const messages = this.validationMessages[key];
+          
+          for (const errorKey in abstractControl.errors) {
+            if (errorKey) {
+              this.formErrors[key] += messages[errorKey] + ' ';
+            }
+          }
+        }
+      }
+    })
+  }
+
+  onLoadDataClick(): void {
+    this.logValidationErrors(this.registerForm);
+    console.log(this.formErrors)
   }
 
   registerNextStep(){
@@ -31,6 +94,10 @@ export class RegisterComponent implements OnInit {
 
   radioChangedHandler(event: any){
     this.accountType = event.target.value;
+  }
+
+  onSubmit(){
+    console.log(this.registerForm.value);
   }
 
 }
