52637288972dfea936900eca6994ce8d2389e6cf
my friend <melhajjami8@boscdelacoma.cat>
2020-08-05 14:04:09 +0200

    validation de login i restore fet, login nomes falta redirecció

--- HubnRock/src/app/authentication/login/login.component.ts
+++ HubnRock/src/app/authentication/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { HttpCommunicationService } from 'src/app/reusable/httpCommunicationService/http-communication.service';
 import { first } from 'rxjs/operators';
 
@@ -12,21 +12,63 @@ export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
 
+  validationMessages = {
+    'email': {
+      'required': 'Introdueix un correu electrònic.',
+      'email': 'Introdueix un correu electrònic vàlid.',
+    },
+    'password': {
+      'required': 'Introdueix una contrasenya.',
+      'minlength': 'Contrasenya massa curta.'
+    }
+  };
+
+
+  formErrors = {
+    'email': '',
+    'password': ''
+  };
+
   constructor(private httpCommunication: HttpCommunicationService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
+      password: ['', [Validators.required, Validators.minLength(6)]]
     });
 
+    this.loginForm.valueChanges.subscribe(value => {
+      this.logValidationErrors(this.loginForm)
+    });
+  }
 
+  logValidationErrors(group: FormGroup = this.loginForm): void {
+    Object.keys(group.controls).forEach((key: string) => {
+      const abstractControl = group.get(key);
+
+      this.formErrors[key] = '';
+      if (abstractControl && !abstractControl.valid &&
+        (abstractControl.touched || abstractControl.dirty)) {
+        const messages = this.validationMessages[key];
+
+        for (const errorKey in abstractControl.errors) {
+          if (errorKey) {
+            this.formErrors[key] += messages[errorKey] + ' ';
+          }
+        }
+      
+      }
+
+      if (abstractControl instanceof FormGroup) {
+        this.logValidationErrors(abstractControl);
+      }
+
+    })
   }
 
   onLogin(): void {
-    console.log(this.loginForm.value)
     // console.log("lo que passem" + this.loginForm.controls.email.value, this.loginForm.get(['password']).value)
-    this.httpCommunication.login(this.loginForm.controls.email.value, '3024j113024j11')
+    this.httpCommunication.login(this.loginForm.controls.email.value, this.loginForm.controls.password.value)
       .pipe(first())
       .subscribe(
         data => {
@@ -53,7 +95,7 @@ export class LoginComponent implements OnInit {
 
   }
 
-  onLoadDataClick(){
+  onLoadDataClick() {
     console.log("=========================================")
     console.log(this.loginForm.value)
   }
