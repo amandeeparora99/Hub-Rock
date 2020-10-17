2645ff586676ab01942b7591d357c7ef62544440
Aman <aaman.arora99@gmail.com>
2020-10-17 16:31:13 +0200

    Spinner wip

--- HubnRock/src/app/http-interceptor.service.ts
+++ HubnRock/src/app/http-interceptor.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpResponse } from '@angular/common/http';
 import { HttpCommunicationService } from './reusable/httpCommunicationService/http-communication.service';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,17 +11,38 @@ export class HttpInterceptorService implements HttpInterceptor {
 
   constructor(private _httpService: HttpCommunicationService) { }
 
-  intercept(req, next) {
-    var currentUser = JSON.parse(this._httpService.getCurrentUser());
-    if (currentUser && currentUser.token) {
-      var reqWithToken = req.clone({
-        setHeaders: {
-          Authorization: `Bearer ${currentUser.token}`
-        }
-      })
-      return next.handle(reqWithToken)
-    } else {
-      return next.handle(req)
-    }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    console.log("SPINNER INICIAT")
+    return this.handler(next, req);
   }
+
+  handler(next, request){
+    return next.handle(request)
+      .pipe(
+        tap(
+          (event) => {
+            if (event instanceof HttpResponse) {
+              console.log("SPINNER PARAT")
+              var currentUser = JSON.parse(this._httpService.getCurrentUser());
+              if (currentUser && currentUser.token) {
+                var reqWithToken = request.clone({
+                  setHeaders: {
+                    Authorization: `Bearer ${currentUser.token}`
+                  }
+                })
+                return next.handle(reqWithToken)
+              } else {
+                return next.handle(request)
+              }
+            }
+          },
+          (error: HttpErrorResponse) => {
+            console.log("SPINNER RESETEJAT")
+            throw error;
+          }
+        ),
+    );
+  }
+
+
 }
