358a1e5044c349677d4daa502b5ab03ca057dcc0
Aman <aaman.arora99@gmail.com>
2021-01-05 18:51:12 +0100

    text changes, botó crear repte footer

--- HubnRock/src/app/reusable/footer/footer.component.ts
+++ HubnRock/src/app/reusable/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/user';
+import { HttpCommunicationService } from '../httpCommunicationService/http-communication.service';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() { }
+  public currentUserObject: User;
+  public currentUser;
+  public userLogged: Boolean = false;
+  
+  constructor(private _httpService: HttpCommunicationService) {
+  }
 
   ngOnInit(): void {
+    this.userLoggedIn();
+    this._httpService.currentUser.subscribe(
+      data => {
+        if (this.userLogged == false) {
+          this.userLoggedIn()
+        }
+        
+        this.currentUserObject = data;
+      }
+    );
+
+  }
+
+  userLoggedIn() {
+    if (this._httpService.loggedIn()) {
+      this.currentUser = JSON.parse(this._httpService.getCurrentUser());
+      this.userLogged = true;
+    } else {
+      this.userLogged = false;
+    }
+
+  }
+
+  obrirTest() {
+    document.getElementById("openCreaRepte").click();
   }
 
 }
