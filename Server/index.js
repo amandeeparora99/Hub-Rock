03838af8ee1994fa1ef55fbbe45fcf80564f7218
Aman <aaman.arora99@gmail.com>
2021-10-13 19:28:07 +0200

    Server mail

--- Server/index.js
+++ Server/index.js
@@ -138,12 +138,12 @@ app.post("/enviarMail", function (req, res) {
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.log(error);
-      res.send({
+      res.sendStatus({
         data: "error",
       });
     } else {
       console.log("Correu enviat " + info.response);
-      res.send({
+      res.sendStatus({
         data: "success",
       });
     }
